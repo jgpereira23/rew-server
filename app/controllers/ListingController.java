e803fa3a1c51f426b6381873ae126f081165c9c1
Mario DeLaPaz <mariocd10@gmail.com>
2018-03-10 09:58:08 -0500

    updated action methods to be CRUD operations - working on create method

--- app/controllers/ListingController.java
+++ app/controllers/ListingController.java
@@ -3,23 +3,31 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.validation.Constraint;
+
+import controllers.Application.SignUp;
 import models.PropType;
 import models.SaleType;
 import models.entity.Listing;
 import models.payload.ActiveListingsResponseVO;
 import models.payload.AddressVO;
 import models.payload.ListingVO;
+import play.data.Form;
+import play.data.FormFactory;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import util.ConversionUtil;
-import views.html.index;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class ListingController extends Controller {
+	
+	@Inject
+	FormFactory formFactory;
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -31,14 +39,16 @@ public class ListingController extends Controller {
     	ActiveListingsResponseVO activeListingsResponse = new  ActiveListingsResponseVO();
     	List<ListingVO> listingsVO = new ArrayList<ListingVO>();
     	ListingVO listingVO = new ListingVO();
+    	//configure address obj
     	AddressVO address = new AddressVO();
     	address.setAptNum("5A");
     	address.setCity("Woodbridge");
     	address.setState("NJ");
     	address.setStreetName("Olive");
-    	address.setStreetNum("535");
+    	address.setStreetNum(535);
     	address.setStreetType("Place");
-    	address.setZip("07095");
+    	address.setZip(07095);
+    	//create listing object
     	listingVO.setAddress(address);
     	listingVO.setBasementFinished(true);
     	listingVO.setBathrooms(2.0);
@@ -50,29 +60,33 @@ public class ListingController extends Controller {
     	listingVO.setPropType(PropType.HALF_DUPLEX);
     	listingVO.setSaleType(SaleType.BY_OWNER);
     	listingVO.setYrBuilt(1953);
+    	//request for active listings and save to a list
     	List<Listing> listings = Listing.findAllActiveListings();
+    	//iterate thru list and add it to our listingsVO list
     	for(Listing listing : listings){
         	listingsVO.add(ConversionUtil.toListingVO(listing) );
     	}
+    	
 		activeListingsResponse.setListings(listingsVO );
     	
 	    return ok(Json.toJson(activeListingsResponse));
     }
     
-    public Result upsertListing() {
-		return null;
-    	
-    }
-    
-    public Result removeListing() {
-        return ok(index.render("Your new application is ready."));
-    }
-    
-    public Result retrieveListing() {
-        return ok(index.render("Your new application is ready."));
-    }
-    public static Result r() {
+    public Result create() {
+    	Listing newlisting = formFactory.form(Listing.class).bindFromRequest().get();
+    	Listing existingListing = Listing.findByID(addressov);
     	return ok("Success!");
     }
-
+    
+    public Result delete(Long id) {
+    	return TODO;
+    }
+    
+    public Result read(Long id) {
+    	return TODO;
+    }
+    
+    public Result update(Long id) {
+    	return TODO;
+    }
 }
